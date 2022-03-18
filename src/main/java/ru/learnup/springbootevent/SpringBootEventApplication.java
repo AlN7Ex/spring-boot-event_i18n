ddd12943b3e1da03d7e3993b688939fc109fd775
Alexey <demon1c1@rambler.ru>
2022-03-18 11:03:06 +0300

    need fix showing ru lang

--- src/main/java/ru/learnup/springbootevent/SpringBootEventApplication.java
+++ src/main/java/ru/learnup/springbootevent/SpringBootEventApplication.java
@@ -5,16 +5,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import ru.learnup.springbootevent.event.MyEventPublisher;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class SpringBootEventApplication {
 
     public static void main(String[] args) {
        ConfigurableApplicationContext context = SpringApplication.run(SpringBootEventApplication.class, args);
+       Locale locale = Locale.getDefault();
 
-        MyEventPublisher publisher = context.getBean("myEventPublisher", MyEventPublisher.class);
-        publisher.publishEvent("some");
+       MyEventPublisher publisher = context.getBean("myEventPublisher", MyEventPublisher.class);
+       publisher.publishEvent("some");
+
+       ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+       messageSource.setBasenames("lang/text");
+
+       System.out.println(messageSource.getMessage("task", null, locale));
     }
 
 }
