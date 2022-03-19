44ae6f883abcc4517ce502472d47ac8792d39ad6
Alexey <alexmisak@yandex.ru>
2022-03-19 11:48:59 +0300

    Done

--- src/main/java/ru/learnup/springbootevent/SpringBootEventApplication.java
+++ src/main/java/ru/learnup/springbootevent/SpringBootEventApplication.java
@@ -1,29 +1,54 @@
 package ru.learnup.springbootevent;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import ru.learnup.springbootevent.event.MyEventPublisher;
 
 import java.util.Locale;
+import java.util.Random;
+import java.util.ResourceBundle;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class SpringBootEventApplication {
 
-    public static void main(String[] args) {
-       ConfigurableApplicationContext context = SpringApplication.run(SpringBootEventApplication.class, args);
-       Locale locale = Locale.getDefault();
-
-       MyEventPublisher publisher = context.getBean("myEventPublisher", MyEventPublisher.class);
-       publisher.publishEvent("some");
-
-       ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-       messageSource.setBasenames("lang/text");
-
-       System.out.println(messageSource.getMessage("task", null, locale));
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("lang/text");
+        return messageSource;
     }
 
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(SpringBootEventApplication.class, args);
+        Locale locale = Locale.getDefault(); // or Locale.US
+
+        Scanner input = new Scanner(System.in);
+        Random rand = new Random();
+        int findMe = rand.nextInt(1000 + 1);
+        System.out.println(findMe);
+
+        MyEventPublisher publisher = context.getBean("myEventPublisher", MyEventPublisher.class);
+
+        publisher.publishEvent(context.getMessage("greetings", null, locale));
+        publisher.publishEvent(context.getMessage("task", null, locale));
+
+        while (true) {
+            int num = input.nextInt();
+            if (findMe > num) {
+                publisher.publishEvent(context.getMessage("greaterNumber", null, locale));
+            } else if (findMe < num) {
+                publisher.publishEvent(context.getMessage("lessNumber", null, locale));
+            } else {
+                publisher.publishEvent(context.getMessage("guessedNumber", null, locale) + " " + findMe);
+                break;
+            }
+        }
+    }
 }
